3f93e42f7f14863f8fe86a951fc21ca70d5f8943
Abhimanyu Singh <74541810+124015001@users.noreply.github.com>
2021-11-30 21:39:50 +0530

    Create 2-tweet-without-media.py

---
+++ changing-source-label/codes/2-tweet-without-media.py
@@ -0,0 +1,12 @@
+import tweepy 
+# personal details 
+my_consumer_key ="customer_key"
+my_consumer_secret ="secret_no"
+my_access_token ="token_no"
+my_access_token_secret ="secret_token"
+# authentication of consumer key and secret 
+my_auth = tweepy.OAuthHandler(my_consumer_key, my_consumer_secret) 
+# Authentication of access token and secret 
+my_auth.set_access_token(my_access_token, my_access_token_secret) 
+my_api = tweepy.API(my_auth)
+my_api.update_status(status="Your desired tweet”)
