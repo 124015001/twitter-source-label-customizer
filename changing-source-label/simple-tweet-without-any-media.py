96857cd6250c69313b7e4b8df4e6e7bd96dadfb3
Abhimanyu Singh <74541810+124015001@users.noreply.github.com>
2021-12-17 15:12:16 +0530

    Delete simple-tweet-without-any-media.py

--- changing-source-label/simple-tweet-without-any-media.py
+++
@@ -1,7 +0,0 @@
-import tweepy
-auth = tweepy.OAuthHandler("CONSUMER KEY HERE", "CONSUMER KEY SECRET HERE")
-auth.set_access_token("ACCESS TOKEN HERE", "ACCESS TOKEN SECRET HERE")
-api = tweepy.API(auth)
-tweet = input("\n")
-api.update_status(status =(tweet))
-print ("You tweet has been tweeted !")
