6e39c0131029636831eaf8f9bd59e37b42c381de
Abhimanyu Singh <74541810+124015001@users.noreply.github.com>
2021-12-19 18:57:04 +0530

    Delete tweet-with-media.py

--- changing-source-label/tweet-with-media.py
+++
@@ -1,8 +0,0 @@
-import tweepy
-auth = tweepy.OAuthHandler("CONSUMER KEY HERE", "CONSUMER KEY SECRET HERE")
-auth.set_access_token("ACCESS TOKEN HERE", "ACCESS TOKEN SECRET HERE")
-api = tweepy.API(auth)
-tweet = input("")
-image="C:/Users/image.jpg"  #Enter image location
-api.update_with_media(image, tweet)
-print ("Your tweet has been tweeted !")
